f7ccca7ea35c5cd3d083521a8b96d6a6c13dda82
Gabriel Masson <g.masson010@gmail.com>
2021-05-23 21:36:01 -0300

    feat: Adds header component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,7 +4,6 @@ import {
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
-import React from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import { Header } from "../components/Header";
+
+const Dashboard: NextPage = () => {
+  return <Header></Header>;
+};
+
+export default Dashboard;
