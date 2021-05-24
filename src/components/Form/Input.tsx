24334a29f8dfc0078b689a9f1e1c41ea44de2e27
Gabriel Masson <g.masson010@gmail.com>
2021-05-23 21:19:33 -0300

    feat: Adds signin page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,34 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export const Input: React.FC<InputProps> = ({ name, label, ...props }) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        type="email"
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        {...props}
+      />
+    </FormControl>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import { NextPage } from "next";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 
-const Index: NextPage = () => {
-  return <h1>Hello Index</h1>;
+import { Input } from "../components/Form/Input";
+
+const SignIn: NextPage = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button tyype="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default Index;
+export default SignIn;
