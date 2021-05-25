4f8ea8ac0f6fd0d17f3e829a30c6748903c18b48
Gabriel Masson <g.masson010@gmail.com>
2021-05-25 00:19:50 -0300

    refactor: Removes unused nav section

--- src/components/SideBar/SidebarNav.tsx
+++ src/components/SideBar/SidebarNav.tsx
@@ -1,11 +1,5 @@
 import { Stack } from "@chakra-ui/react";
-import {
-  RiContactsLine,
-  RiDashboardLine,
-  RiGitMergeLine,
-  RiGroupLine,
-  RiInputMethodLine,
-} from "react-icons/ri";
+import { RiContactsLine, RiDashboardLine, RiGroupLine } from "react-icons/ri";
 
 import { LIST_ROLES, LIST_USERS } from "../../constants/permissions";
 
@@ -26,16 +20,6 @@ export const SidebarNav: React.FC = () => {
 
         <NavLink href="/users" icon={RiContactsLine} permissions={[LIST_USERS]}>
           Usuários
-        </NavLink>
-      </NavSection>
-
-      <NavSection title="Automação">
-        <NavLink href="/forms" icon={RiInputMethodLine}>
-          Formulários
-        </NavLink>
-
-        <NavLink href="/automation" icon={RiGitMergeLine}>
-          Automação
         </NavLink>
       </NavSection>
     </Stack>
